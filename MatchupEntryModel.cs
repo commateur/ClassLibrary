3e74d1730b2ecadc745bc29c9caaffc40c31a39f
commateur <pglevin@gmail.com>
2022-08-12 17:11:02 +0300

    GUI

--- MatchupEntryModel.cs
+++ MatchupEntryModel.cs
@@ -5,6 +5,13 @@
         public TeamModel TeamCompeting { get; set; }
         public double  Score{ get; set; }
         public MatchupModel ParentMatchup { get; set; }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialScore"></param>
+        public MatchupEntryModel(double initialScore)
+        {
+            System.Console.WriteLine();
+        }
     }
 }
