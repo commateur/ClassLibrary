751758ca14c1d00e1c162a136100211bea8f4fc4
commateur <pglevin@gmail.com>
2022-08-12 12:29:38 +0300

    Add project files.

---
+++ MatchupModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public string CellPhoneNumber { get; set; }
+
+
+    }
+}
